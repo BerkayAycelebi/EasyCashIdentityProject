aec1e6da5b71a7f8f10a086d68674966b599e078
riza.aycelebi <differentiate.function@gmail.com>
2023-08-03 16:00:49 +0300

    Identity Register Backend

---
+++ EasyCashIdentityProject.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using EasyCashIdentityProject.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentityProject.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentityProject.PresentationLayer.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public readonly UserManager<AppUser> _userManager;
+
+        public RegisterController(UserManager<AppUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Index(AppUserRegisterDtos appUserRegisterDtos)
+        {
+          
+            if(ModelState.IsValid)
+            {
+                AppUser appUser = new AppUser()
+                {
+                    UserName=appUserRegisterDtos.Username,
+                    Name=appUserRegisterDtos.Name,
+                    Surname=appUserRegisterDtos.Surname,
+                    Email=appUserRegisterDtos.Email,
+
+                };
+                var result=await _userManager.CreateAsync(appUser,appUserRegisterDtos.Password);
+                
+                if(result.Succeeded)
+                {
+                    return RedirectToAction("Index", "ConfirmMail");
+                }
+                
+            }
+            return View();
+        }
+    }
+}
